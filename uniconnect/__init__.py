4f1eb6dd3c2bc6b33e82792bdcbf58dc211ae6e4
shashi kundan <rk683420@gmail.com>
2026-07-02 06:50:02 +0530

    v0.1.1: author metadata, expanded keywords, license-files, classifiers, documentation URLs

--- uniconnect/__init__.py
+++ uniconnect/__init__.py
@@ -1,3 +1,7 @@
+__version__ = "0.1.1"
+__author__ = "Shashi Kundan"
+__email__ = "shashikundan0001@gmail.com"
+
 from uniconnect.core.factory import connect, get_connector
 from uniconnect.core.registry import registry
 from uniconnect.core.base import BaseConnector
